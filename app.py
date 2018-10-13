698f37b8da7590026411f5a85c7689e791e78c66
stufi04 <st.ivanov44@gmail.com>
2018-10-13 16:17:56 +0200

    make it better

---
+++ app.py
@@ -0,0 +1,146 @@
+from flask import Flask, request, make_response, url_for
+from flask_pymongo import PyMongo
+from flask import abort, jsonify, redirect, render_template
+from forms import ProductForm
+from bson.objectid import ObjectId
+from flask import render_template
+import json
+import bson
+from flask_login import LoginManager, current_user, login_required
+from flask_login import login_user, logout_user
+from forms import LoginForm
+from model import User
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'enydM2ANhdcoKwdVa0jWvEsbPFuQpMjf' # Create your own.
+app.config['SESSION_PROTECTION'] = 'strong'
+
+app.config['MONGO_DBNAME'] = 'foodb'
+#app.config['MONGO_URI'] = 'mongodb://localhost:27017/foodb'
+app.config['MONGO_URI'] = "mongodb://stufi04:ss1304@ds119110.mlab.com:19110/ads"
+
+mongo = PyMongo(app)
+
+login_manager = LoginManager()
+login_manager.setup_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+  """Flask-Login hook to load a User instance from ID."""
+  u = mongo.db.users.find_one({"username": user_id})
+  if not u:
+    return None
+  return User(u['username'])
+
+
+@app.route('/')
+def index():
+  return redirect(url_for('products_list'))
+
+
+@app.route('/products/')
+def products_list():
+  """Provide HTML listing of all Products."""
+  # Query: Get all Products objects, sorted by date.
+  products = mongo.db.products.find()[:]
+  return render_template('product/index.html',
+    products=products)
+
+@app.route('/products/<product_id>/')
+@login_required
+def product_detail(product_id):
+  """Provide HTML page with a given product."""
+  # Query: get Product object by ID.
+  product = mongo.db.products.find_one({ "_id": ObjectId(product_id) })
+  print product
+  if product is None:
+    # Abort with Not Found.
+    abort(404)
+  return render_template('product/detail.html',
+    product=product)
+
+@app.route(
+  '/products/<product_id>/edit/',
+  methods=['GET', 'POST'])
+@login_required
+def product_edit(product_id):
+  product = mongo.db.products.find_one({"_id": ObjectId(product_id)})
+  if product is None:
+    # Abort with Not Found.
+    abort(404)
+  form = ProductForm(request.form)
+  if request.method == 'GET':
+    form.name.data = product.get('name')
+    form.description.data = product.get('description')
+    form.price.data = product.get('price')
+    return render_template('product/edit.html', form=form)
+  elif request.method == 'POST' and form.validate():
+    mongo.db.products.replace_one({'name': product.get('name')}, form.data)
+    return redirect(url_for('products_list'))
+
+
+@app.route('/products/<product_id>/delete/', methods=['DELETE'])
+@login_required
+def product_delete(product_id):
+  """Delete record using HTTP DELETE, respond with JSON."""
+  result = mongo.db.products.delete_one({ "_id": ObjectId(product_id) })
+  if result.deleted_count == 0:
+    # Abort with Not Found, but with simple JSON response.
+    response = jsonify({'status': 'Not Found'})
+    response.status = 404
+    return response
+  return jsonify({'status': 'OK'})
+
+
+@app.route('/products/create/', methods=['GET', 'POST'])
+@login_required
+def product_create():
+  """Provide HTML form to create a new product."""
+  form = ProductForm(request.form)
+  if request.method == 'POST' and form.validate():
+    mongo.db.products.insert_one(form.data)
+    # Success. Send user back to full product list.
+    return redirect(url_for('products_list'))
+  # Either first load or validation error at this point.
+  return render_template('product/edit.html', form=form)
+
+@app.errorhandler(404)
+def error_not_found(error):
+  return render_template('error/not_found.html'), 404
+
+@app.errorhandler(bson.errors.InvalidId)
+def error_not_found(error):
+  return render_template('error/not_found.html'), 404
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+  if current_user.is_authenticated:
+    return redirect(url_for('products_list'))
+  form = LoginForm(request.form)
+  error = None
+  if request.method == 'POST' and form.validate():
+    username = form.username.data.lower().strip()
+    password = form.password.data.lower().strip()
+    user = mongo.db.users.find_one({"username": form.username.data})
+    if user and User.validate_login(user['password'], form.password.data):
+      user_obj = User(user['username'])
+      login_user(user_obj)
+      return redirect(url_for('products_list'))
+    else:
+      error = 'Incorrect username or password.'
+  return render_template('user/login.html',
+      form=form, error=error)
+
+@app.route('/logout/')
+def logout():
+  logout_user()
+  return redirect(url_for('products_list'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
